494522f35c324b6416e5346ab7a3b9420b88c45a
Hemanthingithub <139162508+Hemanthingithub@users.noreply.github.com>
2023-07-10 23:32:09 +0530

    Add files via upload

---
+++ data/index.js
@@ -0,0 +1,11 @@
+import users from './users.json';
+import flights from './flights.json';
+import bookings from './bookings.json';
+import responseSuccess from './response-success.json';
+
+export default {
+    users,
+    flights,
+    bookings,
+    responseSuccess
+}
